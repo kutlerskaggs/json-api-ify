44d10d6ae18e2df9125f104dcb2e0ad4996c2103
cludden <chris.ludden@gmail.com>
2016-02-18 17:13:38 -0700

    progress

--- README.md
+++ README.md
@@ -1,2 +1,135 @@
 # json-api-ify
-coming soon...
+a `node.js v5+` framework agnostic library for serializing your data to [JSON API v1.0](http://jsonapi.org/) compliant payloads, inspired by [jsonapi-serializer](https://github.com/SeyZ/jsonapi-serializer).
+
+
+## Install
+```bash
+npm install --save json-api-ify
+```
+
+## Getting Started
+Create a new *reusable* serializer.
+```javascript
+var Serializer = require('json-api-ify');
+
+let serializer = new Serializer({
+    baseUrl: 'https://www.example.com/api',
+    meta: {
+        'api-version': 'v1.0.0'
+    }
+});
+```
+
+
+Define a type.
+```javascript
+serializer.define('users', {
+    id: '_id',
+    blacklist: [
+        'password',
+        'nested.secret.attribute'
+    ],
+    links: {
+        self(resource, options, cb) {
+            let link = options.baseUrl + '/users/' + resource.id;
+            cb(null, link);
+        }
+    },
+    meta: {
+        nickname(resource, options, cb) {
+            let nickname = 'lil ' + resource.attributes.first;
+            cb(null, nickname);
+        }
+    },
+    processResource(resource, cb) {
+        return cb(null, resource.toObject());
+    },
+    topLevelLinks: {
+        self(options, cb) {
+            let link = options.baseUrl + '/users';
+            cb(null, link);
+        },
+        next(options, cb) {
+            let link = options.baseUrl + '/users';
+            if (options.nextPage) {
+                link += '?page=' + options.nextPage;
+            }
+            cb(null, link);
+        }
+    },
+    topLevelMeta: {
+        total(options, cb) {
+            cb(null, options.total);
+        }
+    }
+}, function(err) {
+    // check for definition errors
+})
+```
+
+Use it.
+```javascript
+var mongoose = require('mongoose'),
+    userSchema = new mongoose.Schema({}, {strict: false}),
+    User = mongoose.model('User', userSchema);
+
+let data = [new User({
+    id: '54735750e16638ba1eee59cb',
+    first: 'Kendrick',
+    last: 'Lamar',
+    email: 'klamar@example.com',
+    password: 'elkjqe0920oqhvrophepohiwveproihgqp398yr9pq8gehpqe9rf9q8er'
+    phone: {
+        home: '+18001234567',
+        mobile: '+180045678910'
+    },
+    address: {
+        addressLine1: '406 Madison Court',
+        zipCode: '49426',
+        country: 'USA'
+    },
+}), new User({
+    id: '5490143e69e49d0c8f9fc6bc',
+    first: 'Kanye',
+    last: 'West',
+    phone: {
+        home: '+18002345678',
+        mobile: '+18007890123'
+    },
+    address: {
+        addressLine1: '361 Shady Lane',
+        zipCode: '23185',
+        country: 'USA'
+    }
+})];
+
+serializer.serialize('users', data, function(err, payload) {
+    console.log(payload);
+});
+
+// or in a route
+function(req, res) {
+    async.waterfall([
+        function findUsers(fn) {
+            User.find({})
+                .limit(10)
+                .skip(parseInt(req.query.page || 0) * 10)
+                .exec(fn);
+        },
+
+        function serialize(users, fn) {
+            serializer.serialize('users', users, {
+                nextPage: (req.query.page || 0) + 1
+            }, fn);
+        }
+    ], function(err, payload) {
+        if (err) {
+            return res.json(500, {errors: [{
+                status: 500,
+                detail: err.message
+            }]});
+        }
+        res.json(200, payload);
+    });
+}
+```
--- test/basic.test.js
+++ test/basic.test.js
@@ -2,7 +2,8 @@
 
 var async = require('async'),
     chai = require('chai'),
-    JsonApiIfy = require('../index'),
+    Serializer = require('../index'),
+    queryString = require('query-string'),
     _ = require('lodash');
 
 let expect = chai.expect;
@@ -15,7 +16,7 @@ describe('basic tests', function() {
         email: 'ttebow@example.com',
         phone: {
             home: null,
-            cell: '+18001234567'
+            mobile: '+18001234567'
         },
         groups: [{
             name: 'admins',
@@ -31,7 +32,7 @@ describe('basic tests', function() {
         email: 'kwest@example.com',
         phone: {
             home: null,
-            cell: '+18001234567'
+            mobile: '+18001234567'
         },
         groups: [{
             name: 'users',
@@ -39,30 +40,82 @@ describe('basic tests', function() {
         }]
     }];
 
-    let json = new JsonApiIfy({
-        baseUrl: 'http://localhost:8080'
+    let serializer = new Serializer({
+        baseUrl: 'https://www.example.com',
+        includeSerializationTime: true
     });
 
+    // configure serializer
     before(function(done) {
-        async.parallel([
-            function(fn) {
-                json.define('users', {
+        async.auto({
+            user: function defineDefaultUserSchema(fn) {
+                serializer.define('users', {
                     id: '_id',
                     blacklist: [
                         'email',
                         'phone.home'
                     ],
+                    links: {
+                        self(resource, options, cb) {
+                            let link = options.baseUrl + '/api/users/' + resource.id;
+                            cb(null, link);
+                        }
+                    },
+                    meta: {
+                        test: function(resource, options, cb) {
+                            cb(null, 'test-' + resource.id);
+                        }
+                    },
                     relationships: {
                         groups: {
                             type: 'groups',
-                            include: true
+                            include: true,
+                            links: {
+                                self(resource, options, cb) {
+                                    let link = options.baseUrl + '/api/users/' + resource.id + '/groups';
+                                    cb(null, link);
+                                },
+                                related(resource, options, cb) {
+                                    let link = options.baseUrl + '/api/users/' + resource.id + '/relationships/groups';
+                                    cb(null, link);
+                                }
+                            }
+                        }
+                    },
+                    topLevelLinks: {
+                        self(options, cb) {
+                            let link = options.baseUrl + '/api/users',
+                                query = _.get(options, 'request.query');
+                            if (query) {
+                                link += '?' + queryString.stringify(query);
+                            }
+                            cb(null, link);
+                        },
+                        next(options, cb) {
+                            let link = options.baseUrl + '/api/users',
+                                query = _.get(options, 'request.query') || {},
+                                next = _.get(options, 'request.nextKey');
+                            if (next) {
+                                query['page[cursor]'] = next;
+                                link += '?' + queryString.stringify(query);
+                            } else {
+                                link = undefined;
+                            }
+                            cb(null, link);
+                        }
+                    },
+                    topLevelMeta: {
+                        'api-version': 'v1.3.9',
+                        total: function(options, cb) {
+                            let total = _.get(options, 'request.total');
+                            cb(null, total);
                         }
                     }
                 }, fn);
             },
 
-            function(fn) {
-                json.define('users', 'public', {
+            userPublic: function definePublicUserSchema(fn) {
+                serializer.define('users', 'public', {
                     whitelist: [
                         'first',
                         'last'
@@ -70,34 +123,241 @@ describe('basic tests', function() {
                 }, fn);
             },
 
-            function(fn) {
-                json.define('groups', {
+            group: function defineDefaultGroupSchema(fn) {
+                serializer.define('groups', {
                     id: 'name',
+                    links: {
+                        self(resource, options, cb) {
+                            let link = options.baseUrl + '/api/groups/' + resource.id;
+                            cb(null, link);
+                        }
+                    },
                     relationships: {
                         users: {
                             type: 'users',
                             schema: 'public',
-                            include: true
+                            include: true,
+                            links: {
+                                self(resource, options, cb) {
+                                    let link = options.baseUrl + '/api/groups/' + resource.id + '/users';
+                                    cb(null, link);
+                                },
+                                related(resource, options, cb) {
+                                    let link = options.baseUrl + '/api/groups/' + resource.id + '/relationships/users';
+                                    cb(null, link);
+                                }
+                            }
+                        }
+                    },
+                    topLevelLinks: {
+                        self(options, cb) {
+                            let link = options.baseUrl + '/api/groups';
+                            cb(null, link);
                         }
                     }
                 }, fn);
-            }
-        ], done);
+            },
+        }, done);
     });
 
-    it('should blacklist the appropriate items', function(done) {
-        json.serialize('users', dataset, function(err, payload) {
-            console.log(JSON.stringify(err), JSON.stringify(payload));
-            expect(err).to.not.exist;
-            expect(payload).to.have.property('data');
-            expect(payload.data).to.have.lengthOf(2);
-            payload.data.forEach(function(resource) {
-                expect(resource.id).to.be.a('number');
-                expect(resource.type).to.equal('users');
-                expect(resource.attributes).to.have.all.keys('first', 'last', 'phone');
-                expect(resource.attributes.phone).to.not.have.keys('home');
+    context('with no options', function() {
+        let err, payload;
+
+        before(function(done) {
+            serializer.serialize('users', dataset, function(e, p) {
+                err = e;
+                payload = p;
+                done(e);
             });
-            done();
+        });
+
+        it('should not error', function() {
+            expect(err).to.not.exist;
+        });
+
+        it('shoud return a valid payload', function() {
+            expect(payload).to.be.an('object').and.contain.all.keys('links', 'data', 'included', 'meta');
+        });
+
+        it('should return the correct top level links', function() {
+            expect(payload.links).to.have.property('self', 'https://www.example.com/api/users');
+            expect(payload.links).to.not.have.property('next');
+        });
+
+        it('should return the correct top level meta', function() {
+            expect(payload.meta).to.have.property('serializationTime').that.is.a('string');
+            expect(payload.meta).to.not.have.property('total');
+            expect(payload.meta).to.have.property('api-version', 'v1.3.9');
+        });
+
+        it('should include 2 serialized users', function() {
+            expect(payload.data).to.have.lengthOf(2);
+            expect(_.map(payload.data, 'id')).to.eql([1,2]);
+            expect(_.map(payload.data, 'type')).to.eql(['users', 'users']);
+            payload.data.forEach(function(resource) {
+                expect(resource).to.be.an('object')
+                    .and.contain.all.keys('type', 'id', 'attributes')
+                    .and.contain.any.keys('meta', 'links', 'relationships');
+            });
+        });
+
+        it('should blacklist the appropriate attributes', function() {
+            payload.data.forEach(function(resource) {
+                expect(resource.attributes).to.contain.all.keys('first', 'last');
+                expect(resource.attributes).to.have.deep.property('phone.mobile');
+                expect(resource.attributes).to.not.have.property('email');
+                expect(resource.attributes).to.not.have.deep.property('phone.home');
+            });
+        });
+
+        it('each resource should include the correct relationships', function() {
+            let tim = payload.data[0],
+                kanye = payload.data[1];
+            expect(tim).to.have.property('relationships').that.is.an('object').with.property('groups');
+            expect(tim.relationships.groups).to.be.an('object').with.all.keys('links', 'data', 'meta');
+            expect(tim.relationships.groups.data).to.have.lengthOf(2);
+            tim.relationships.groups.data.forEach(function(rel) {
+                expect(rel).to.be.an('object').with.all.keys('id', 'type', 'links', 'meta');
+            });
+            expect(_.map(tim.relationships.groups.data, 'id')).to.eql(['admins', 'users']);
+
+            expect(kanye).to.have.property('relationships').that.is.an('object').with.property('groups');
+            expect(kanye.relationships.groups).to.be.an('object').with.all.keys('links', 'data', 'meta');
+            expect(kanye.relationships.groups.data).to.have.lengthOf(1);
+            kanye.relationships.groups.data.forEach(function(rel) {
+                expect(rel).to.be.an('object').with.all.keys('id', 'type', 'links', 'meta');
+            });
+            expect(_.map(kanye.relationships.groups.data, 'id')).to.eql(['users']);
+        });
+
+        it('each resource should include the correct meta', function() {
+            payload.data.forEach(function(resource) {
+                expect(resource).to.have.property('meta').that.is.an('object').with.keys('test');
+                expect(resource.meta.test).to.equal('test-' + resource.id);
+            });
+        });
+
+        it('should include the correct related resources in the `included` attribute', function() {
+            expect(payload.included).to.be.an('array').with.lengthOf(2);
+            expect(_.map(payload.included, 'id')).to.contain('admins').and.contain('users');
+        });
+    });
+
+    context('with request options', function() {
+        let err, payload;
+
+        before(function(done) {
+            serializer.serialize('users', dataset, {
+                request: {
+                    query: {
+                        id: 'lte(20)'
+                    },
+                    nextKey: 10,
+                    total: 1000
+                }
+            }, function(e, p) {
+                err = e;
+                payload = p;
+                done(e);
+            });
+        });
+
+        it('should not error', function() {
+            expect(err).to.not.exist;
+        });
+
+        it('should return a valid payload', function() {
+            expect(payload).to.be.an('object').and.contain.all.keys('links', 'data', 'included', 'meta');
+        });
+
+        it('should return the correct top level links', function() {
+            expect(payload.links).to.have.property('self').that.contains('https://www.example.com/api/users?');
+            expect(payload.links).to.have.property('next').that.contains('page').and.contains('cursor');
+        });
+
+        it('should return the correct top level meta', function() {
+            expect(payload.meta).to.have.property('serializationTime').that.is.a('string');
+            expect(payload.meta).to.have.property('total', 1000);
+            expect(payload.meta).to.have.property('api-version', 'v1.3.9');
+        });
+
+        it('should include 2 serialized users', function() {
+            expect(payload.data).to.have.lengthOf(2);
+            expect(_.map(payload.data, 'id')).to.eql([1,2]);
+            expect(_.map(payload.data, 'type')).to.eql(['users', 'users']);
+            payload.data.forEach(function(resource) {
+                expect(resource).to.be.an('object')
+                    .and.contain.all.keys('type', 'id', 'attributes')
+                    .and.contain.any.keys('meta', 'links', 'relationships');
+            });
+        });
+
+        it('should blacklist the appropriate attributes', function() {
+            payload.data.forEach(function(resource) {
+                expect(resource.attributes).to.contain.all.keys('first', 'last');
+                expect(resource.attributes).to.have.deep.property('phone.mobile');
+                expect(resource.attributes).to.not.have.property('email');
+                expect(resource.attributes).to.not.have.deep.property('phone.home');
+            });
+        });
+    });
+
+    context('with non default schema', function() {
+        let err, payload;
+
+        before(function(done) {
+            serializer.serialize('users', 'public', dataset, {
+                request: {
+                    query: {
+                        id: 'lte(20)'
+                    },
+                    nextKey: 10,
+                    total: 1000
+                }
+            }, function(e, p) {
+                err = e;
+                payload = p;
+                done(e);
+            });
+        });
+
+        it('should not error', function() {
+            expect(err).to.not.exist;
+        });
+
+        it('shoud return a valid payload', function() {
+            expect(payload).to.be.an('object').and.contain.all.keys('links', 'data', 'included', 'meta');
+        });
+
+        it('should return the correct top level links', function() {
+            expect(payload.links).to.have.property('self').that.contains('https://www.example.com/api/users?');
+            expect(payload.links).to.have.property('next').that.contains('page').and.contains('cursor');
+        });
+
+        it('should return the correct top level meta', function() {
+            expect(payload.meta).to.have.property('serializationTime').that.is.a('string');
+            expect(payload.meta).to.have.property('total', 1000);
+            expect(payload.meta).to.have.property('api-version', 'v1.3.9');
+        });
+
+        it('should include 2 serialized users', function() {
+            expect(payload.data).to.have.lengthOf(2);
+            expect(_.map(payload.data, 'id')).to.eql([1,2]);
+            expect(_.map(payload.data, 'type')).to.eql(['users', 'users']);
+            payload.data.forEach(function(resource) {
+                expect(resource).to.be.an('object')
+                    .and.contain.all.keys('type', 'id', 'attributes')
+                    .and.contain.any.keys('meta', 'links', 'relationships');
+            });
+        });
+
+        it('should blacklist the appropriate attributes', function() {
+            payload.data.forEach(function(resource) {
+                expect(resource.attributes).to.have.keys('first', 'last');
+                expect(resource.attributes).to.not.have.deep.property('phone.mobile');
+                expect(resource.attributes).to.not.have.property('email');
+                expect(resource.attributes).to.not.have.deep.property('phone.home');
+            });
         });
     });
 });
